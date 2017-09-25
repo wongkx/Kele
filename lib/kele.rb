4fec763e6271e3e164e1964b189e3f3b943bedfa
Kevin Wong <kevinwong6175@gmail.com>
2017-09-25 02:15:28 +0000

    User story 2 - Retrieve Users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,17 +1,18 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
 
     def initialize(e, p)
         auth = { "email": e, "password": p }.to_json
-        
         headers = { :content_type => 'application/json'}
-        
         response = self.class.post('https://www.bloc.io/api/v1/sessions', {:body => auth, :headers => headers} )
-
         @auth_token = response["auth_token"]
-        puts response
-        puts @auth_token
+    end
+    
+    def get_me
+        response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+        @user_hash = JSON.parse(response.body)
     end
 end
