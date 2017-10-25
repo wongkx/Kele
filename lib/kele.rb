c0be62851a800e4efeabad3b75058c0bf5462c95
wongkx <kevinwong6175@gmail.com>
2017-10-24 22:24:15 -0500

    User story 6 - Checkpoint submission (#6)

--- lib/kele.rb
+++ lib/kele.rb
@@ -42,4 +42,16 @@ class Kele
         }, headers: { "authorization" => @auth_token })
         @message = JSON.parse(response.body)
     end
+  
+    def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+        url = "https://www.bloc.io/api/v1/checkpoint_submissions"
+        response = self.class.post(url, body: {
+            "assignment_branch": assignment_branch,
+            "assignment_commit_link": assignment_commit_link,
+            "checkpoint_id": checkpoint_id,
+            "comment": comment,
+            "enrollment_id": enrollment_id
+        }, headers: { "authorization" => @auth_token })
+        @message = JSON.parse(response.body)
+    end
 end
